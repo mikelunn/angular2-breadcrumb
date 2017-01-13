4a296991b7baa7f9dbc4af0dddbd3a28a7b4d732
V Pena <victor@vpena.com>
2017-01-13 14:06:28 -0500

    added Bootstrap and modified HTML

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -1,6 +1,7 @@
 /**
- * Created by complunm on 9/8/2016.
+ * Created by Mike Lunn on 9/8/2016.
  */
+
 import {Routes, RouterModule} from "@angular/router";
 import {ModuleWithProviders} from "@angular/core";
 import {HomeComponent} from "./home/home.component";
@@ -9,9 +10,9 @@ import {CustomerDetailComponent} from "./customer-detail/customer-detail.compone
 
 const appRoutes : Routes = [
   {path:'', redirectTo:'/home', pathMatch:'full'},
-  {path:'home', component:HomeComponent, data:{title:'HOME'}},
-  {path:'customer', component:CustomerComponent, data:{title:'CUSTOMER'}},
-  {path:'customer-detail', component:CustomerDetailComponent, data:{title:'DETAIL'}}
+  {path:'home', component:HomeComponent, data:{title:'Home'}},
+  {path:'customer', component:CustomerComponent, data:{title:'Customer'}},
+  {path:'customer-detail', component:CustomerDetailComponent, data:{title:'Detail'}}
 ]
 export const routingProviders : any[] = [];
 export const routes : ModuleWithProviders = RouterModule.forRoot(appRoutes);
